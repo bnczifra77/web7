f15e16b6c6f2312421c12612acdcfbddd2a8aa09
bnczifra77 <bnczifra77@gmail.com>
2025-07-25 12:12:27 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
